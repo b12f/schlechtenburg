a6b162eddb392aae6e770f049cb94a95f32eb2e4
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2022-03-22 23:42:03 +0100

    Docs done different

--- packages/docgen/support/generate-type-md.mjs
+++
@@ -1,9 +0,0 @@
-const generateTypeMd = (docs) => {
-  if (docs.type === 'union') {
-    return docs.types.map(type => generateTypeMd(type)).join('|');
-  }
-  
-  return docs.name;
-};
-
-export default generateTypeMd;
--- packages/docs/lib/api/package.ts
+++
@@ -1,4 +0,0 @@
-export const getShortPackageName = (name: string) => {
-  const parts = name.split('/');
-  return parts[1] || parts[0] || name;
-}
